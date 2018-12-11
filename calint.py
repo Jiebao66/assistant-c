556ca7361aefa7abc8f102939882cda6b471b62b
primy <primelyw@qq.com>
2018-12-11 20:28:37 +0800

    bug

--- calint.py
+++ calint.py
@@ -1,6 +1,6 @@
 gfr  = ['data/in/simple_data.in','data/in/middle_data.in','data/in/hard_data.in']
 gfw1 = ['data/out/int/simple_data.out','data/out/int/middle_data.out','data/out/int/hard_data.out']
-gfw2 = ['data/out/float/simple_data.out','data/out/float/middle_data.out','data/out/float/hard_data.out']
+gfw2 = ['data/out/int/simple_data.out','data/out/int/middle_data.out','data/out/int/hard_data.out']
 
 
 
